71b4d9d13e3116be6c13c7ac29419ccbe835dfaa
Kirbyszsr <luobocun@qq.com>
2021-05-04 21:58:42 +0800

    [20210503]add function for check available versions

--- pypi_crawler/pypi_analyzer.py
+++ pypi_crawler/pypi_analyzer.py
@@ -7,17 +7,20 @@ import re
 class PyPIAnalyzer:
     def __init__(self, basic_info: dict):
         self.basic_info = basic_info
+        self.requirement_list = []
 
-    def available_versions(self,version_str_list: list = []):
+    def available_versions(self, version_str_list=None):
+        if version_str_list is None:
+            version_str_list = []
         if not version_str_list:
             return self.basic_info["released_versions"]
         available_versions = [Version(version) for version in self.basic_info["released_versions"]]
         checked_versions = []
         symbols = ['<', '<=', '>', '>=', '==', '!=']
         versions = [[], [], [], [], [], []]
-        version_dict = dict(zip(symbols,versions))
+        version_dict = dict(zip(symbols, versions))
         for version_str in version_str_list:
-            elements = re.findall("([!><=]+)([0-9].*)",string=version_str)[0]
+            elements = re.findall("([!><=]+)([0-9].*)", string=version_str)[0]
             for symbol in symbols:
                 if elements[0] == symbol:
                     version_dict[symbol].append(Version(elements[1]))
@@ -47,20 +50,57 @@ class PyPIAnalyzer:
 
     def parse_requirement_dist(self):
         requirements_list = self.basic_info["requires_dist"]
-        return requirements_list
+        if not requirements_list:
+            return []
+        parsed_requirements_list = []
+        for requirement in requirements_list:
+            requirement_index = requirement.find(';')
+            if requirement_index != -1:
+                parsed_requirements_list.append(requirement[:requirement_index])
+            else:
+                parsed_requirements_list.append(requirement)
+        parsed_requirements_imports = []
+        for parsed_requirement in parsed_requirements_list:
+            elements = re.findall(
+                "(.+) \((.+)\)", parsed_requirement)
+            if elements:
+                for element in elements:
+                    parsed_requirements_imports.append(
+                        Import(name=element[0].strip(),
+                               filename=self.basic_info["name"],
+                               line=-1,
+                               from_element=element[0].strip(),
+                               import_element="*",
+                               as_element=element[0].strip(),
+                               version=element[1].strip()
+                               )
+                    )
+            else:
+                parsed_requirements_imports.append(
+                    Import(name=parsed_requirement.strip(),
+                           filename=self.basic_info["name"],
+                           line=-1,
+                           from_element=parsed_requirement.strip(),
+                           import_element="*",
+                           as_element=parsed_requirement.strip(),
+                           version='*'
+                           )
+                )
+                self.requirement_list = parsed_requirement
+        return parsed_requirements_list
 
 
 if __name__ == "__main__":
-    package_names = ['django']
+    package_names = ['django', 'pip', 'requests']
     for package_name in package_names:
         path = './output/%s/' % package_name
         requirement_simple_filename = path + 'requirement-simple.json'
         with open(requirement_simple_filename) as f:
             print("[pypi_analyzer]Open file: %s" % requirement_simple_filename)
             result = json.load(f)
-            print(result)
+            # print(result)
         analyzer = PyPIAnalyzer(result)
         print("[pypi_analyzer]Available versions:")
-        print(analyzer.available_versions(['>=3.0']))
+        print(analyzer.available_versions([]))
         print("[pypi_analyzer]parse_requirement_dist:")
         print(analyzer.parse_requirement_dist())
